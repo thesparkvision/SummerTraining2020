ea850f8a266566e3379bc61e0d65cdbb20cad4c3
thesparkvision <aman15.pandya2gmail.com>
2020-05-20 22:32:15 +0530

    Successfully ran first Java program

---
+++ Day1/firstProgram.java
@@ -0,0 +1,12 @@
+/*Author-Aman Pandya
+  Date practiced- 20 May 2020
+  Topic- First program in Java
+*/
+
+class Demo
+{
+	public static void main(String [] args)
+	{
+		System.out.println("Hello students, how are you? ");
+	}
+}
